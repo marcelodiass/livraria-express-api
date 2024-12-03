1f6740cbf035de5b18e9b561ef5c624fdd223c7b
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2024-12-03 14:34:37 -0300

    init commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "Curso de node.js",
+    "/livros": "Rota de livros",
+    "/autores" : "Rota de autores"
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+    console.log("Server running at http://localhost:3000/");
+})
