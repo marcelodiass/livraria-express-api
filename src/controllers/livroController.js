1dea7994ef53b0a71f3fd18b192a5e41306ffd6d
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2024-12-05 17:35:11 -0300

    fix: refatoração completa do CRUD de livros

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/Livro.js";
 class LivroController {
 
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros);
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros);
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao listar livros", error: error.message });
+        }
+    }
+
+    static async listarLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao listar livro por id", error: error.message });
+        }
     }
 
     static async cadastrarLivro(req, res) {
@@ -13,6 +27,26 @@ class LivroController {
             res.status(201).json({ message: "Livro criado com sucesso!", livro: novoLivro });
         } catch (error) {
             res.status(500).json({ message: "Erro ao criar livro", error: error.message });
+        }
+    }
+
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado com sucesso!" });
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao atualizar livro", error: error.message });
+        }
+    }
+
+    static async deletarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({ message: "Livro deletado com sucesso!" });
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao deletar livro", error: error.message });
         }
     }
 };
