8ed6ebe258f58f5d8f5a095d3c08ddf1b36187b5
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2024-12-09 21:57:03 -0300

    feat: busca por parametros (editora)

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -53,6 +53,16 @@ class LivroController {
             res.status(500).json({ message: "Erro ao deletar livro", error: error.message });
         }
     }
+
+    static async listarLivrosPorEditora(req, res) {
+        const editora = req.query.editora;
+        try {
+            const livrosPorEditora = await livro.find({ editora: editora});
+            res.status(200).json(livrosPorEditora);
+        } catch (error) {
+            res.status(500).json({ message: "Erro ao listar livros por editora", error: error.message });
+        }
+    }
 };
 
 export default LivroController;
